0f4bb93fa510459bb5ead8c0b0fc6966b0e62210
HackInvent <HackInvent@users.noreply.github.com>
2016-08-10 16:59:37 +0200

    Create tinker-button.ino

---
+++ tinker-button.ino
@@ -0,0 +1,12 @@
+#include <tinkerlib.h>
+
+#define TINKER_UNO_SHIELD   //on utilise le module TINKER_UNO_SHIELD
+#define TINKER_BUTTON       //on utilise le module TINKER_BUTTON
+
+setup(){
+
+}
+
+loop(){
+
+}
